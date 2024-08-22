3879a095b1d3e32f5f5ae39cf7a22d487d2669bc
Shawn Abraham <johnabe5605@gmail.com>
2024-08-22 12:05:30 -0500

    Added Extreme Bonus

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args){
+        String username = System.getProperty("user.name");
+        System.out.println("Hello, " + username + "!");
+    }
+    
+}
+
