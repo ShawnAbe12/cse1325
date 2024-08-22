d4fe21ef81dd2dc51b6c34f6dcae7d80ce55e5b6
Shawn Abraham <johnabe5605@gmail.com>
2024-08-22 11:59:14 -0500

    removed the encoding var

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class Hello {
     public static void main(String[] args){
-        String encoding = "UTF-8";
-        Scanner in = new Scanner(System.in, encoding);
+        Scanner in = new Scanner(System.in);
         System.out.print("What is your name? ");
         String output = in.nextLine();
         System.out.println("Hello " + output);
