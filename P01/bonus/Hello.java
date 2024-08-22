b68e78f6d50ed2ed216f38904de888f9a378855b
Shawn Abraham <johnabe5605@gmail.com>
2024-08-22 09:32:58 -0500

    Added Bonus Answer

---
+++ P01/bonus/Hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String output = in.nextLine();
+        System.out.println("Hello " + output);
+    }
+    
+}
+
