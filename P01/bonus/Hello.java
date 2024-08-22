f94cc7ca7a99f9158d9115e3591fcead2eabf064
Shawn Abraham <johnabe5605@gmail.com>
2024-08-22 11:57:30 -0500

    Added bonus Answers

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Hello {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
+        String encoding = "UTF-8";
+        Scanner in = new Scanner(System.in, encoding);
+        System.out.print("What is your name? ");
         String output = in.nextLine();
         System.out.println("Hello " + output);
     }
