1e55b076ab4f62844b64eaffe4d564894efbe970
Shawn Abraham <johnabe5605@gmail.com>
2024-08-31 12:30:24 -0500

    Expanding denomination class

--- P02/bonus/Coin.java
+++ P02/bonus/Coin.java
@@ -8,18 +8,7 @@ public class Coin {
     }
 
     public double getValue(){
-        switch (denomination.toString()) {
-            case "PENNY":
-                return 0.01;
-            case "NICKEL":
-                return 0.05;
-            case "DIME":
-                return 0.10;
-            case "QUARTER":
-                return 0.25;
-
-        }
-        return 0;
+        return denomination.getValue();
     }
     public int getYear(){
         return year;
