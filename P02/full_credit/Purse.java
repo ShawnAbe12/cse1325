2a22fa14919c994e5fd8a9e336cfd19f5885c6ef
Shawn Abraham <johnabe5605@gmail.com>
2024-08-31 11:23:05 -0500

    Forgot to private denom, and year in coin.java so fixed that

--- P02/full_credit/Purse.java
+++ P02/full_credit/Purse.java
@@ -6,12 +6,6 @@ public class Purse {
       public static void main(String[] args) { 
       double totalSaved = 0;
       // Scanner scanner = new Scanner();
-      // Denomination myVar1 = Denomination.PENNY; 
-      // Denomination myVar2 = Denomination.NICKEL; 
-      // Denomination myVar3 = Denomination.DIME; 
-      // Denomination myVar4 = Denomination.QUARTER; 
-      // Denomination myVar5 = Denomination.QUARTER; 
-
 
       Denomination[] denominationsArray = {Denomination.PENNY,Denomination.NICKEL,Denomination.DIME,Denomination.QUARTER,Denomination.QUARTER};
       Coin[] coinArray = new Coin[5];
@@ -21,11 +15,8 @@ public class Purse {
       for(Denomination denom : denominationsArray){
         coinArray[count] = new Coin(denom, yearArray[count]);
         count++;
-
       }
       
-
-      // coinArray[0]= new Coin(myVar5, 2021);
       int minYear = coinArray[0].getYear();
       int maxYear = coinArray[0].getYear();
       for(Coin coin:coinArray){
