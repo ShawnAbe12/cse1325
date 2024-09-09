bf393d68fb65244c2f47afbc77e18db9da038114
Shawn Abraham <johnabe5605@gmail.com>
2024-09-09 16:11:14 -0500

    adding the bonus test vectors and fixing full credit throw and catch statements in student file

--- P03/Student.java
+++ P03/Student.java
@@ -2,22 +2,26 @@ public class Student{
     private String name;
     private int id;
     private String email;
-    private Account account = new Account();
+    private Account account;
 
     public Student(String name,int id,String email){
-        try {
-            if(email.endsWith("@uta.edu") != true && email.endsWith("mavs.uta.edu") != true ){
-                throw new IllegalArgumentException("\"Non-UTA email address: "+ email);
-            }
-        }
-        catch(Exception e) {
-            System.err.println(e.getMessage());
-            System.exit(-1);    
+        // try {
+        //     if(email.endsWith("@uta.edu") != true && email.endsWith("mavs.uta.edu") != true ){
+        //         throw new IllegalArgumentException("\"Non-UTA email address: "+ email);
+        //     }
+        // }
+        // catch(Exception e) {
+        //     System.err.println(e.getMessage());
+        //     System.exit(-1);    
+        // }
+        if(email.endsWith("@uta.edu") != true && email.endsWith("mavs.uta.edu") != true ){
+            throw new IllegalArgumentException("\"Non-UTA email address: "+ email);
         }
         
         this.name = name;
         this.id = id; 
         this.email = email;
+        this.account = new Account();
     }
     public String requestMedia(Media media){
         return account.play(media);
--- P03/TestAccount.java
+++ P03/TestAccount.java
@@ -1,9 +1,13 @@
 public class TestAccount{
     public static void main(String[] args){
-        Account acc = new Account();
-        String title = "The Little Shop of Horrors";
-        String url = "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0";
-        Media media = new Media(title, url);
-        System.out.println(acc.play(media));
+        Account acc1 = new Account();
+        Account acc2 = new Account();
+        if(acc1.getAccountNumber() !=1 || acc2.getAccountNumber() !=2)
+        {
+            System.out.println("FAIL");
+            System.out.println("Expected outcome:\nAccount number 1: 1\nAccount number 2: 2\n"+ "Actual outcome:\nAccount number 1: " +acc1.getAccountNumber() + "\nAccount number 2: " + acc2.getAccountNumber());
+            System.exit(-1);
+        }
+        System.out.println("here");
     }
 }
