70b1a7c708599cb36145e931517ac9a383b710da
Shawn Abraham <johnabe5605@gmail.com>
2024-09-17 00:53:46 -0500

    added the java docs for Media and adjusted build.xml

--- P04/product/Media.java
+++ P04/product/Media.java
@@ -1,3 +1,11 @@
+/**
+ * Access a variety of Videos, Music, and Podcast Legally!!
+ * 
+ * @author Shawn Abraham
+ * @version 1.0
+ * @since 1.0
+ * 
+ */
 package product;
 
 public class Media {
@@ -5,7 +13,15 @@ public class Media {
     private String url;
     private int points;
 
-
+    /**
+     * Creates a Media Isntance
+     *
+     * @param title
+     * @param url
+     * @param points
+     * 
+     * @since 1.0
+     */
     public Media(String title, String url,int points) {
         this.title = title;
         this.url = url;
@@ -18,6 +34,24 @@ public class Media {
         }
         
     }
+    /**
+     * Returns the number of points a Media cost
+     * 
+     * @return number of points
+     * 
+     * @since 1.0
+     */
+    public int getPoint(){
+        return points;
+    }
+
+    /**
+     * Prints Media's base output
+     * 
+     * @return title of media, url of media, and points of media
+     * 
+     * @since 1.0 
+     */
     @Override
     public String toString() {
         return title + " (" + url + ") Points Remaining: " + points;
